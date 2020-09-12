fa57db84fb5d8746f286d81dd9ea3a069da051b7
amazingTest <523314409@qq.com>
2020-09-12 16:03:01 +0800

    [fix]修复html正则验证

--- backend/testframe/interfaceTest/tester.py
+++ backend/testframe/interfaceTest/tester.py
@@ -9,6 +9,7 @@ from bson import ObjectId
 from threading import Thread
 
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
@@ -36,8 +37,8 @@ class tester:
             from app import nlper
             self.nlper = nlper
         except ImportError as e:
-            pass
-            # raise ImportError('nlp模型导入失败！<%s>' % e)
+            # pass
+            raise ImportError('nlp模型导入失败！<%s>' % e)
 
         self.test_case_list = test_case_list
         self.domain = domain
@@ -67,7 +68,7 @@ class tester:
             if 'lastManualTestResult' in test_case:
                 test_case.pop('lastManualTestResult')
             domain = test_case["domain"] if 'domain' in test_case and isinstance(test_case["domain"], str) and \
-                not test_case["domain"].strip() == '' else self.domain
+                                            not test_case["domain"].strip() == '' else self.domain
             if 'requestProtocol' in test_case and 'route' in test_case:
                 url = '%s://%s%s' % (test_case['requestProtocol'].lower(), domain, test_case['route'])
                 test_case["url"] = url
@@ -91,7 +92,7 @@ class tester:
             if 'lastManualTestResult' in test_case:
                 test_case.pop('lastManualTestResult')
             domain = test_case["domain"] if 'domain' in test_case and isinstance(test_case["domain"], str) and \
-                not test_case["domain"].strip() == '' else self.domain
+                                            not test_case["domain"].strip() == '' else self.domain
             if 'requestProtocol' in test_case and 'route' in test_case:
                 url = '%s://%s%s' % (test_case['requestProtocol'].lower(), domain, test_case['route'])
                 test_case["url"] = url
@@ -135,51 +136,59 @@ class tester:
         set_global_vars = None  # for example {'status': ['status']}
 
         domain = test_case["domain"] if 'domain' in test_case and isinstance(test_case["domain"], str) and \
-                        not test_case["domain"].strip() == '' else self.domain
-        if 'requestProtocol' in test_case and 'route' in test_case:
-            test_case['route'] = \
-                common.resolve_global_var(pre_resolve_var=test_case['route'], global_var_dic=self.global_vars) \
-                    if isinstance(test_case['route'], str) else test_case['route']
-            url = '%s://%s%s' % (test_case['requestProtocol'].lower(), domain, test_case['route'])
+                                        not test_case["domain"].strip() == '' else self.domain
 
-        if 'requestMethod' in test_case:
-            method = test_case['requestMethod']
+        try:
 
-        if 'presendParams' in test_case and isinstance(test_case['presendParams'], dict):
-            
-            # dict 先转 str，方便全局变量替换
-            test_case['presendParams'] = str(test_case['presendParams'])
+            if 'requestProtocol' in test_case and 'route' in test_case:
+                test_case['route'] = \
+                    common.resolve_global_var(pre_resolve_var=test_case['route'], global_var_dic=self.global_vars) \
+                        if isinstance(test_case['route'], str) else test_case['route']
+                url = '%s://%s%s' % (test_case['requestProtocol'].lower(), domain, test_case['route'])
 
-            # 全局替换
-            test_case['presendParams'] = common.resolve_global_var(pre_resolve_var=test_case['presendParams'],
-                                                                   global_var_dic=self.global_vars)
+            if 'requestMethod' in test_case:
+                method = test_case['requestMethod']
 
-            # 转回 dict
-            test_case['presendParams'] = ast.literal_eval(test_case['presendParams'])
-            
-            json_data = test_case['presendParams']
+            if 'presendParams' in test_case and isinstance(test_case['presendParams'], dict):
+                # dict 先转 str，方便全局变量替换
+                test_case['presendParams'] = str(test_case['presendParams'])
 
-        if 'headers' in test_case and not test_case['headers'] in ["", None, {}, {'': ''}]:
-            if isinstance(test_case['headers'], list):
-                for header in test_case['headers']:
-                    if not header['name'].strip() == '':
-                        headers[header['name']] = \
-                            common.resolve_global_var(pre_resolve_var=header['value'], global_var_dic=self.global_vars)\
-                            if isinstance(header['value'], str) else headers[header['name']]
-            else:
-                raise TypeError('headers must be list!')
+                # 全局替换
+                test_case['presendParams'] = common.resolve_global_var(pre_resolve_var=test_case['presendParams'],
+                                                                       global_var_dic=self.global_vars)
 
-        if 'setGlobalVars' in test_case and not test_case['setGlobalVars'] in [[], {}, "", None]:
-            set_global_vars = test_case['setGlobalVars']
+                # 转回 dict
+                test_case['presendParams'] = ast.literal_eval(test_case['presendParams'])
 
-        headers = None if headers == {} else headers
+                json_data = test_case['presendParams']
 
-        test_case['cookies'] = []
-        for key, value in session.cookies.items():
-            cookie_dic = dict()
-            cookie_dic['name'] = key
-            cookie_dic['value'] = value
-            test_case['cookies'].append(cookie_dic)
+            if 'headers' in test_case and not test_case['headers'] in ["", None, {}, {'': ''}]:
+                if isinstance(test_case['headers'], list):
+                    for header in test_case['headers']:
+                        if not header['name'].strip() == '':
+                            headers[header['name']] = \
+                                common.resolve_global_var(pre_resolve_var=header['value'],
+                                                          global_var_dic=self.global_vars) \
+                                    if isinstance(header['value'], str) else headers[header['name']]
+                else:
+                    raise TypeError('headers must be list!')
+
+            if 'setGlobalVars' in test_case and not test_case['setGlobalVars'] in [[], {}, "", None]:
+                set_global_vars = test_case['setGlobalVars']
+
+            headers = None if headers == {} else headers
+
+            test_case['cookies'] = []
+            for key, value in session.cookies.items():
+                cookie_dic = dict()
+                cookie_dic['name'] = key
+                cookie_dic['value'] = value
+                test_case['cookies'].append(cookie_dic)
+
+        except BaseException as e:
+            returned_data["status"] = 'failed'
+            returned_data["testConclusion"].append('测试前置准备失败, 错误信息: <%s> ' % e)
+            return returned_data
 
         try:
 
@@ -189,13 +198,14 @@ class tester:
             if test_case['requestMethod'].lower() == 'get':
                 response = session.request(url=url, method=method, params=json_data, headers=headers, verify=False)
             else:
-                response = session.request(url=url, method=method, json=json_data, headers=headers, verify=False) if use_json_data\
+                response = session.request(url=url, method=method, json=json_data, headers=headers,
+                                           verify=False) if use_json_data \
                     else session.request(url=url, method=method, data=json_data, headers=headers, verify=False)
-
+            # print(response.headers) TODO 请求头断言
         except BaseException as e:
-           returned_data["status"] = 'failed'
-           returned_data["testConclusion"].append('请求失败, 错误信息: <%s> ' % e)
-           return returned_data
+            returned_data["status"] = 'failed'
+            returned_data["testConclusion"].append('请求失败, 错误信息: <%s> ' % e)
+            return returned_data
 
         test_case['headers'] = headers  # 重新赋值生成报告时用
 
@@ -204,8 +214,10 @@ class tester:
         returned_data["responseData"] = response.text
 
         try:
+
             response_json = json.loads(response.text) if isinstance(response.text, str) \
                                                          and response.text.strip() else {}
+
         except BaseException as e:
 
             if set_global_vars and isinstance(set_global_vars, list):
@@ -218,30 +230,68 @@ class tester:
 
             if 'checkHttpCode' in test_case and not test_case['checkHttpCode'] in ["", None]:
                 check_http_code = test_case['checkHttpCode']
-            
+
             if check_http_code and not str(response_status_code) == str(check_http_code):
                 returned_data["status"] = 'failed'
                 returned_data["testConclusion"].append('响应状态码错误, 期待值: <%s>, 实际值: <%s>。\t'
                                                        % (check_http_code, response_status_code))
                 return returned_data
-            
-            is_check_res_data_valid = isinstance(test_case.get('checkResponseData'), list) and\
+
+            is_check_res_data_valid = isinstance(test_case.get('checkResponseData'), list) and \
                                       len(list(filter(lambda x: str(x.get('regex')).strip() == '',
                                                       test_case.get('checkResponseData')))) < 1
-            is_check_res_similarity_valid = isinstance(test_case.get('checkResponseSimilarity'), list) and\
+            is_check_res_similarity_valid = isinstance(test_case.get('checkResponseSimilarity'), list) and \
                                             len(list(filter(lambda x: isinstance(x.get('targetSimilarity'), type(None)),
                                                             test_case.get('checkResponseSimilarity')))) < 1
-            is_check_res_number_valid = isinstance(test_case.get('checkResponseNumber'), list) and\
+            is_check_res_number_valid = isinstance(test_case.get('checkResponseNumber'), list) and \
                                         len(list(filter(lambda x: str(x.get('expressions').get('expectResult')).strip()
                                                                   == '', test_case.get('checkResponseNumber')))) < 1
+            if is_check_res_data_valid:
+                if 'checkResponseData' in test_case and not test_case['checkResponseData'] in [[], {}, "", None]:
+                    if not isinstance(test_case['checkResponseData'], list):
+                        raise TypeError('checkResponseData must be list！')
+                    for index, crd in enumerate(test_case['checkResponseData']):
+                        if not isinstance(crd, dict) or 'regex' not in crd or 'query' not in crd or \
+                                not isinstance(crd['regex'], str) or not isinstance(crd['query'], list):
+                            raise TypeError('checkResponseData is not valid!')
+
+                        # TODO 可开启/关闭 全局替换
+                        test_case['checkResponseData'][index]['regex'] = \
+                            common.resolve_global_var(pre_resolve_var=crd['regex'], global_var_dic=self.global_vars) if \
+                                crd.get('regex') and isinstance(crd.get('regex'), str) else ''  # 警告！python判断空字符串为False
+
+                    check_response_data = test_case['checkResponseData']
+                    if check_response_data:
+                        try:
+                            for crd in check_response_data:
+                                regex = crd['regex']
+                                if regex.strip() == '':
+                                    continue
+                                query = crd['query']
+                                # query 支持全局变量替换
+                                for index, single_query in enumerate(query):
+                                    query[index] = common.resolve_global_var(pre_resolve_var=single_query,
+                                                                             global_var_dic=self.global_vars)
+                                result = re.search(regex, str(response.text))  # python 将regex字符串取了r''(原生字符串)
+                                if not result:
+                                    returned_data["status"] = 'failed'
+                                    returned_data["testConclusion"].append('判断响应值错误(查询语句为: %s),    响应值应满足正则: <%s>,\
+                                                                                实际值: <%s> (%s)。(正则匹配时会将数据转化成string)\t'
+                                                                           % (
+                                                                           query, regex, response.text, type(response.text)))
+                        except BaseException as e:
+                            returned_data["status"] = 'failed'
+                            returned_data["testConclusion"].append('判断响应值时报错, 错误信息: <%s>。\t' % e)
+
             # TODO 目前默认当 is_check_res_similarity_valid 和  is_check_res_number_valid 为真时，返回格式必须可转 json ，可优化
-            is_test_failed = is_check_res_data_valid or is_check_res_number_valid or is_check_res_similarity_valid
+            is_test_failed = is_check_res_number_valid or is_check_res_similarity_valid
 
             returned_data['status'] = 'failed' if is_test_failed else 'ok'
 
             returned_data["testConclusion"].append('服务器返回格式不是json, 错误信息: %s, 服务器返回为: %s '
                                                    % (e, response.text)) if returned_data.get('status') and \
-                                                                    returned_data.get('status') == 'failed' else None
+                                                                            returned_data.get(
+                                                                                'status') == 'failed' else None
             if returned_data['status'] == 'ok':
                 returned_data["testConclusion"].append('测试通过')
 
@@ -279,7 +329,7 @@ class tester:
             for index, crs in enumerate(test_case['checkResponseSimilarity']):
                 if not isinstance(crs, dict) or 'baseText' not in crs or 'targetSimilarity' not in crs \
                         or 'compairedText' not in crs or not isinstance(crs['baseText'], str) \
-                         or not isinstance(crs['compairedText'], str):
+                        or not isinstance(crs['compairedText'], str):
                     raise TypeError('checkResponseSimilarity is not valid!')
                 test_case['checkResponseSimilarity'][index]['baseText'] = \
                     common.resolve_global_var(pre_resolve_var=crs['baseText'], global_var_dic=self.global_vars) if \
@@ -327,6 +377,10 @@ class tester:
                     if regex.strip() == '':
                         continue
                     query = crd['query']
+                    # query 支持全局变量替换
+                    for index, single_query in enumerate(query):
+                        query[index] = common.resolve_global_var(pre_resolve_var=single_query,
+                                                                 global_var_dic=self.global_vars)
                     real_value = common.dict_get(response_json, query)
                     if real_value is None:
                         returned_data["status"] = 'failed'
@@ -373,7 +427,8 @@ class tester:
                         returned_data["status"] = 'failed'
                         returned_data["testConclusion"].append('相似度校验未达标！已对比字符串: 「%s」、「%s」, 实际相似度: 「%s」 '
                                                                '预期相似度: 「%s」。\t ' % (base_text, compaired_text,
-                                                                              actual_similarity, target_similarity))
+                                                                                    actual_similarity,
+                                                                                    target_similarity))
             except BaseException as e:
                 returned_data["status"] = 'failed'
                 returned_data["testConclusion"].append('判断相似度时报错, 模型服务器可能已宕机/断网。具体错误信息: <%s>。\t' % e)
@@ -392,7 +447,7 @@ class tester:
             test_result = common.format_response_in_dic(test_result)
             self.test_result_list[index] = test_result
             testing_case_model.update({"_id": ObjectId(test_case_id)},
-                               {'$set': {'lastManualTestResult': test_result}})
+                                      {'$set': {'lastManualTestResult': test_result}})
 
     def send_report(self, test_report_model, project_id, executor_nick_name, execution_mode):
         test_count = len(self.test_result_list)
